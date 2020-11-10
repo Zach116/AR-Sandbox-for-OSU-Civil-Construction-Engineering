a1c96c2327b54194ee01eb97be7b897d4cbc7aca
Zachary Bishop <zach.bishop116@gmail.com>
2020-11-10 12:14:21 -0800

    Updated Sprint 1 Document

---
+++ src/Unity Tutorial/Assets/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;    
+
+    // Update is called once per frame
+    // Use FixedUpdate when utilizing physics
+    void FixedUpdate() {
+        // Make sure to multiply by Time.deltaTime so its independent of frame rate
+        rb.AddForce(0, 0, 500 * Time.deltaTime);
+    }
+}
